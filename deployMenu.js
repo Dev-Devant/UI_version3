75469acb04b8ea75d6af163f1921fcd0e6bd84f7
Dev-Devant <andeliros@yahoo.com.ar>
2024-10-21 22:04:03 -0300

    fixit deploy failure

--- deployMenu.js
+++ deployMenu.js
@@ -336,23 +336,10 @@ function createPopupMercadoLibre(data) {
       <div class="modal">
         <div class="modal-header">
           <h2>Payment Form</h2>
-                  <button id="BackBilling" class="text-[#bbbec6] hover:text-[#4bc6ff] transition-colors">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-5 w-5">
-                        <polyline points="15 18 9 12 15 6"/> <!-- Flecha hacia la izquierda -->
-                    </svg>
-                  </button>
-
-                  <button id="closeBillinga" class="text-[#bbbec6] hover:text-[#4bc6ff] transition-colors">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-5 w-5">
-                        <line x1="18" x2="6" y1="6" y2="18"/>
-                        <line x1="6" x2="18" y1="6" y2="18"/>
-                    </svg>
-                  </button>        
-                  <script src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
-                    data-preference-id="316205094-246ab804-ab87-4639-9b2a-d6395a7f7a1a" data-source="button">
-                  </script>         
+          <button id="BackBilling">Back</button>
+          <button id="closeBillinga">Close</button>
         </div>
-      
+        <div id="mercadopago-button-container" style="width: 100%; height: 50px; margin-top: 20px;"></div>
       </div>
     </div>
   `;
